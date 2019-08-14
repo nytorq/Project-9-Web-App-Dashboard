6726f9f890f697445dcf75fba5dd71c42d8ae0e5
Charles Prator <cprator@spotx.tv>
2019-08-14 16:02:47 -0600

    JS to get success messages to appear
    - Created the following functions:
      - removeParent(element)
      - displayMessage(e)
      - submit_message()

--- js/script.js
+++ js/script.js
@@ -5,22 +5,47 @@ const submit_buttons = document.querySelectorAll("button[type='submit']");
 
 // FUNCTIONS
 
-function close_element() {
-  for (i=0 ; i < close_icons.length ; i++) {
-    let icons = close_icons[i];
-    icons.addEventListener('click', () => {
-      removeParent(icons);
-    })
-  }
-}
-
 function removeParent(element) {
   let parent = element.parentElement;
   parent.remove();
 }
 
+function displayMessage(e) {
+  let parent = e.parentNode;
+  // if (parent.tagName === "FORM") {
+  //   parent.lastElementChild.style.display = 'inline-block';
+  // } else {
+  //   parent = parent.parentNode;
+  //
+  // }
+  do {
+    let pa
+  } while (parent !=== 'FORM')
+  // e.nextElementSibling.style.display = 'inline-block';
+}
+
+function close_element() {
+  for (i=0 ; i < close_icons.length ; i++) {
+    let icon = close_icons[i];
+    icon.addEventListener('click', () => {
+      removeParent(icon);
+    })
+  }
+}
+
+function submit_message() {
+  for (i=0 ; i < submit_buttons.length ; i++) {
+    let button = submit_buttons[i];
+    button.addEventListener('click', () => {
+      displayMessage(button)
+      // console.log('button clicked!')
+    });
+  }
+}
+
 // FUNCTION DECLARATIONS
 
 close_element();
+submit_message();
 
 // EVENT HANDLERS
