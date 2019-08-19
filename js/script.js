aac6c6c82cf46dc9fdc0f1341bde018bc6404cb0
Charles Prator <cprator@spotx.tv>
2019-08-19 10:35:02 -0600

    Members array and successMessage SCSS
    - Started work on creating an array of the members of the site
    - Added a SCSS transition to make successMessage to appear

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 
 const close_icons = document.getElementsByClassName('fa-times');
 const submit_buttons = document.querySelectorAll("button[type='submit']");
+const members = [
+  {
+    "userID": 0,
+    "firstName" : "Victoria",
+    "lastName"  : "Chambers",
+    "email"     : "vchambers@gmail.com",
+    "joinDate"  : "10/15/15"
+  },
+  {
+    "userID": 21,
+    "firstName" : "Dale",
+    "lastName"  : "Byrd",
+    "email"     : "dbyrd@gmail.com",
+    "joinDate"  : "10/15/15"
+  }
+]
 
 // FUNCTIONS
 
@@ -12,36 +28,20 @@ function removeParent(element) {
 
 function displayMessage(e) {
   let parent = returnForm(e)
-  parent.lastElementChild.style.display = 'inherit';
+  parent.lastElementChild.setAttribute("style", "display: inherit;")
+  window.setTimeout(function() {parent.lastElementChild.style.opacity = 1;}, 200);
+  // parent.lastElementChild.style.display = 'inherit';
+  // parent.lastElementChild.style.transition = 'display 3s ease-in-out';
 }
 
 function returnForm(e) {
   let currentNode = e;
   while (currentNode.tagName !== 'FORM') {
-    // console.log('Current node is ' + e.tagName + '.')
-    // currentNode = e.parentNode;
-    console.log("We're inside the while loop. The currentNode is " +  currentNode.tagName + '.');
+    currentNode.tagName + '.';
     currentNode = currentNode.parentNode
   }
-  console.log('Outside the while loop. The currentNode is ' + currentNode.tagName + '.');
   return currentNode
 }
-  // while (e.parentNode )
-  // let parent = e.parentNode;
-  // // if (parent.tagName === "FORM") {
-  // //   parent.lastElementChild.style.display = 'inline-block';
-  // // } else {
-  // //   parent = parent.parentNode;
-  // //
-  // // }
-  // do {
-  //   let currentNode = e;
-  //   let parent = currentNode.parentNode;
-  //   currentNode = parent;
-  // } while (parent !=== 'FORM')
-  //
-  // while ( !== '')
-  // e.nextElementSibling.style.display = 'inline-block';
 
 function close_element() {
   for (i=0 ; i < close_icons.length ; i++) {
@@ -57,7 +57,6 @@ function submit_message() {
     let button = submit_buttons[i];
     button.addEventListener('click', () => {
       displayMessage(button)
-      // console.log('button clicked!')
     });
   }
 }
